c8a19ba29cda7d5319dc9a74b3f69e58bc9afe0d
MOHIT CHUGH <mohit01chugh@gmail.com>
2025-04-10 12:57:56 +0530

    add

---
+++ 01_Band_Name_Generator/main.py
@@ -0,0 +1,11 @@
+# Welcome message
+print("Welcome to the Band Name Generator.")
+
+# Get city name
+city_name = input("What's the name of the city you grew up in? \n")
+
+# Get pet name
+pet_name = input("What's your pet's name? \n")
+
+# Display band name
+print("Your band name could be: " + city_name + " " + pet_name)
